b17e117b649689cb49cedd03dad7f98931be46a8
Choco-Mint23 <131267685+Choco-Mint23@users.noreply.github.com>
2023-04-24 22:20:14 +0800

    Update dentists.php

--- dentists.php
+++ dentists.php
@@ -1,7 +1,7 @@
 <!-- Navbar -->
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-        <a class="navbar-brand fs-4" href="#">C U Smile</a>
+        <a class="navbar-brand fs-4" href="#"><i class="fa-solid fa-tooth fa-lg" style="color: #ffffff;"></i> &nbsp C U Smile </i></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>

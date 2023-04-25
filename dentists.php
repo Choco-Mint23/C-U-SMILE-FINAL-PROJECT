e730b3bc0c936c64abd92a34fba15eae823c06a6
Jaden-77 <131693026+Jaden-77@users.noreply.github.com>
2023-04-26 02:28:24 +0800

    Update dentists.php

--- dentists.php
+++ dentists.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>List of Dentists</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
+	rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+
+	<header>
 <!-- Navbar -->
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
@@ -20,3 +33,60 @@
             </div>
         </div>
     </nav>
+    </header>
+
+	<main>
+		<section class="container">
+			<h1>List of Dentists</h1>
+
+			<div class="leadDent">
+				<img src="https://via.placeholder.com/400x400" alt="lead Dentist">
+				<h2 style="margin-top: 150px;">Dr. James Reymond</h2>
+				<hr style="width: 80px; border-width: 1px; border-color: black; margin-left: 100px;">
+				<h3 style="margin-top: 20px;">Lead Dentist</h3>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In maxime alias nulla molestiae blanditiis sed, quisquam corrupti reprehenderit quas neque quibusdam harum exercitationem dignissimos sapiente eum doloremque minus quis dolore.</p>
+			</div>
+
+			<div class="row">
+				<div class="col">
+					<img src="https://via.placeholder.com/350x500" alt="Doctor 1">
+					<h2>Dr. John Doe</h2>
+					<h3>Dentist</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor fringilla dui sed posuere. Sed eget nibh consectetur, tincidunt urna sit amet, vehicula mi. Sed eu efficitur tellus. Nullam hendrerit vestibulum ipsum, eu ullamcorper dolor gravida et.</p>
+				</div>
+
+				<div class="col1">
+					<img src="https://via.placeholder.com/350x500" alt="Doctor 2">
+					<h2>Dr. Jane Smith</h2>
+					<h3>Dentist</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor fringilla dui sed posuere. Sed eget nibh consectetur, tincidunt urna sit amet, vehicula mi. Sed eu efficitur tellus. Nullam hendrerit vestibulum ipsum, eu ullamcorper dolor gravida et.</p>
+				</div>
+
+				<div class="col2">
+					<img src="https://via.placeholder.com/350x500" alt="Doctor 1">
+					<h2>Dr. John Doe</h2>
+					<h3>Dentist</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor fringilla dui sed posuere. Sed eget nibh consectetur, tincidunt urna sit amet, vehicula mi. Sed eu efficitur tellus. Nullam hendrerit vestibulum ipsum, eu ullamcorper dolor gravida et.</p>
+				</div>
+
+				<div class="col3">
+					<img src="https://via.placeholder.com/350x500" alt="Doctor 2">
+					<h2>Dr. Jane Smith</h2>
+					<h3>Dentist</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor fringilla dui sed posuere. Sed eget nibh consectetur, tincidunt urna sit amet, vehicula mi. Sed eu efficitur tellus. Nullam hendrerit vestibulum ipsum, eu ullamcorper dolor gravida et.</p>
+				</div>
+			</div>
+
+		</section>
+	</main>
+
+				<footer>
+					<h4>Footer(to be edited)</h4>
+					
+				</footer>
+
+				
+				
+</body>
+</html>
+    

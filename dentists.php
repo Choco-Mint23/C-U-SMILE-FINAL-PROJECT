5d8121616629302fb99bdbe3b5d6a46b7e6e60f2
Jaden-77 <131693026+Jaden-77@users.noreply.github.com>
2023-05-15 01:07:44 +0800

    Updated dentists.php

--- dentists.php
+++ dentists.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>List of Dentists</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="dentists_style.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
 		  rel="stylesheet"  integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
@@ -36,58 +36,86 @@
     </header>
 
 	<main>
+
 		<section class="section1">
+			<h1>Our Dentists</h1>
+			
+			<p>Our dentists are a team of experienced and knowledgeable professionals who are dedicated to delivering exceptional dental care to our patients. With their advanced 
+				techniques and passion for dentistry, our team strives to make every dental visit comfortable and stress-free. We are proud to serve our community with top-quality 
+				dental services. Get to know each of our dentists and their specialties by reading their profiles below.
+			</p>
+		</section>
 
-			<h1>List of Dentists</h1>
-
+		<section class="section2">
 			<div class="leadDentImg">
-				<img src="images\FemaleDentist_1110x700.jpg" alt="lead Dentist">
+				<img src="dentists_images\dentist-malony-santos-new (2).jpg" alt="lead Dentist">
 				</div>
 			<div class="leadDent">
-				<h2>Dr. Sarah Park</h2>
+				<h2>Dr. David Lee</h2>
 				<h3>Lead Dentist</h3>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In maxime alias nulla molestiae blanditiis sed, quisquam corrupti reprehenderit quas neque quibusdam harum exercitationem dignissimos sapiente eum doloremque minus quis dolore.</p>
+				<p> With over 20 years of experience in the field of dentistry, Dr. Lee is a trusted leader in our practice. He is dedicated to providing exceptional 
+					dental care to each of our patients and is committed to staying up-to-date on the latest advancements in dentistry. As the lead dentist, 
+					Dr. Lee oversees all aspects of our practice and ensures that our team delivers the highest quality care to every patient. His warm and caring demeanor, 
+					combined with his expertise, makes him a favorite among our patients.
+				</p>
 			</div>
 		</section>
 
 		
-		<section class="section2">
+		<section class="section3">
 
 			<div class="dent1">
-				<img src="images\drgirishdongarwar.jpg" alt="Doctor 1">
-				<h2>Dr. John Doe</h2>
-				<h3>Dentist</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor fringilla dui sed posuere. Sed eget nibh consectetur, tincidunt urna sit amet, vehicula mi. Sed eu efficitur tellus. Nullam hendrerit vestibulum ipsum, eu ullamcorper dolor gravida et.</p>
+				<img src="dentists_images\drgirishdongarwar.jpg" alt="Doctor 1">
+				<h2>Dr. Radhika Chopra</h2>
+				
+				<h3>Orthodontist </h3>
+				<p>Meet Dr. Radhika Chopra, our orthodontic specialist who is passionate about creating beautiful, healthy smiles for his patients. With his expertise in braces and Invisalign, he can help you achieve the smile you've always wanted.</p>
 			</div>
 
 		    <div class="dent2">
-				<img src="images\dentist-malony-santos-new (1).jpg" alt="Doctor 2">
-				<h2>Dr. David Lee</h2>
-				<h3>Dentist</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor fringilla dui sed posuere. Sed eget nibh consectetur, tincidunt urna sit amet, vehicula mi. Sed eu efficitur tellus. Nullam hendrerit vestibulum ipsum, eu ullamcorper dolor gravida et.</p>
+				<img src="dentists_images\woman-wore-white-shirt-raised-her-right-hand_1150-18810.avif" alt="Doctor 2">
+				<h2>Dr. Sarah Park</h2>
+				<h3>General & Cosmetic Dentist</h3>
+				<p>	Say hello to Dr. Sarah Park, our general dentist who is committed to providing comprehensive dental care for patients of all ages. From routine cleanings to complex restorative procedures, Dr. Garcia will ensure that your smile is healthy and beautiful.</p>
 			</div> 
 		 
 			<div class="dent3">
-				<img src="images\drgirishdongarwar.jpg" alt="Doctor 1">
-				<h2>Dr. Michael Johnson</h2>
-				<h3>Dentist</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor fringilla dui sed posuere. Sed eget nibh consectetur, tincidunt urna sit amet, vehicula mi. Sed eu efficitur tellus. Nullam hendrerit vestibulum ipsum, eu ullamcorper dolor gravida et.</p>
+				<img src="dentists_images\dentist-reza-tavakoli.jpg" alt="Doctor 1">
+				<h2>Dr. Carlito Leus</h2>
+				<h3>Endodontist</h3>
+				<p>Meet Dr. Carlito Leus is our endodontic specialist who focuses on root canal therapy and other complex procedures 
+					to save damaged or infected teeth. With his advanced training and skillful techniques, he can help you preserve 
+					your natural teeth and maintain optimal oral health.
+				</p>
 			</div>
   
 			<div class="dent4">
-				<img src="images\drgirishdongarwar.jpg" alt="Doctor 2">
-				<h2>Dr. James Chen</h2>
-				<h3>Dentist</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor fringilla dui sed posuere. Sed eget nibh consectetur, tincidunt urna sit amet, vehicula mi. Sed eu efficitur tellus. Nullam hendrerit vestibulum ipsum, eu ullamcorper dolor gravida et.</p>
+				<img src="dentists_images\doc-mendoza.jpg" alt="Doctor 2">
+				<h2>Dr. John Kim</h2>
+				<h3>Periodontist</h3>
+				<p>Meet Dr. John Kim, our periodontal specialist who specializes in the diagnosis and treatment of gum disease. 
+					With his knowledge and expertise in advanced techniques like laser therapy, he can help you restore the health 
+					of your gums and prevent future dental problems.
+				</p>
 			</div>  
 		</section>
 
 	</main>
 
-			<footer>
-				<h4>Footer(to be edited)</h4>
-					
-			</footer> 
+		<footer>
+        	<div class="container">
+          	<div class="footer-content">
+            	<div class="logo">
+              	<a class="navbar-brand fs-4" href="#"><i class="fa-solid fa-tooth fa-lg" style="color: #ffffff;"></i> &nbsp C U Smile </a>
+            	</div>
+            	<div class="contact-info">
+              	<p>Email: <a href="mailto:youremail@example.com">CUsmile@gmail.com</a></p>
+              	<p>Phone: <a href="tel:+123456789">+2310878</a></p>
+            	</div>
+          	</div>
+        	</div>
+      	</footer>
 				
 </body>
 </html>
+  

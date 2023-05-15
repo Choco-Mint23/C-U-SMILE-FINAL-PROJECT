4522bce395437cace1b92db1d74080a4e7a2d171
NicSuellz <99799739+NicSuellz@users.noreply.github.com>
2023-05-15 21:42:27 +0800

    Add files via upload

--- aboutUs.php
+++ aboutUs.php
@@ -20,3 +20,64 @@
             </div>
         </div>
     </nav>
+    <html>
+
+    <head>
+
+    <meta charset="UTF-8">
+    <meta htto-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>C U Smile</title>
+    <hr style="height:1px;border-width:0;color:rgb(255, 255, 255);background-color:rgb(255, 255, 255)">
+    
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+
+
+    </head>
+    <body>
+    
+    <div class="about-section">
+        <div style="font-size:20px;padding:10px;margin-bottom:10px;">
+            <h1>About Us</h1>
+            <p>C U Smile Dental Care is a dental clinic located in Sitio Nasipit, Barangay Talamban, Cebu City. We are relatively new and have been in operation for only 2 years but have already built a loyal customer base through our high-quality dental care in a warm and friendly environment. We offer a wide range of services, including routine cleanings, fillings, attractions, and cosmetic procedures like teeth whitening and veneers. Book an appointment now at +63-32-230-0100.</p>
+          
+            <div class="scroll-container">
+                <div class="scroll-container">
+                    <img src="imgs/dental_place.jpg" alt="Cinque Terre" width="600" height="400">
+                    <img src="imgs/teeth-white.JPG" alt="Forest" width="600" height="400">
+                    <img src="imgs/cleaning.JPG" alt="Northern Lights" width="600" height="400">
+                    <img src="imgs/check-up.JPG" alt="Mountains" width="600" height="400">
+                  </div>
+            </div>
+            
+            <hr style="height:1px;border-width:0;color:#65b3b6;background-color:#65b3b6">
+            <div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=san carlos university talamban &t=&z=10&ie=UTF8&iwloc=&output=embed" frameborder="10" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2yu.co">2yu</a><br><style>.mapouter{position:relative;text-align:right;height:100%;width:100%;}</style><a href="https://embedgooglemap.2yu.co/">html embed google map</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:100%;width:100%;}</style></div></div>
+              <div> 
+                <form action = "dentists.php">
+                <button class="button">List of Dentists</button>
+                </form>
+              </div>
+        </div>
+    </div>
+
+    <!-- Footer-->
+    <footer>
+        <div class="container">
+          <div class="footer-content">
+            <div class="logo">
+              <a class="navbar-brand fs-4" href="#"><i class="fa-solid fa-tooth fa-lg" style="color: #ffffff;"></i> &nbsp C U Smile </a>
+            </div>
+            <div class="contact-info">
+              <p>Email: <a href="mailto:youremail@example.com">CUsmile@gmail.com</a></p>
+             <p>Mobile #: <a href="tel:+1234567890">(032) 230 0100</a></p>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+</body>
+</html>
+    
